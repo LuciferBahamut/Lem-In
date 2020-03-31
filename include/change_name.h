0f0742c739cf80f03a5e863e817b37f985d40f23
Chauvin Chloé <Chloe@localhost.localdomain>
2020-03-31 17:28:21 +0200

    [ADD] first push

---
+++ include/change_name.h
@@ -0,0 +1,14 @@
+#ifndef __TRUC_H__
+#define __TRUC_H__
+
+void my_putchar(char c);
+void my_putstr(char const *str);
+int my_strlen(char *const str);
+void write_error(char *str);
+
+#define ERROR 84
+#define SUCCESS 0
+#define TRUE 1
+#define FASLE 0
+
+#endif
